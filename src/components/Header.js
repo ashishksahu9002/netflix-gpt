c70f28821e9e51a03a298f4a0ae128e019e5363f
Ashish Kumar <ashishksahu9002@gmail.com>
2025-08-13 19:16:48 +0530

    bug fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { LOGO_URL, USER_LOGO_URL } from "../utils/constants";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
-        navigate("/");
       })
       .catch((error) => {
         // An error happened.
         console.log(error);
       });
-  };
+    };
+    
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const { uid, email, displayName } = user;
+          dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+          navigate("/browse");
+        } else {
+          dispatch(removeUser());
+          navigate("/");
+      }
+    });
+
+    return ()=> unsubscribe()
+  }, []);
+
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between items-center">
       <img src={LOGO_URL} alt="logo" className="w-44" />
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,14 +8,12 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const name = useRef(null);
@@ -48,7 +46,6 @@ const Login = () => {
               dispatch(
                 addUser({ uid: uid, email: email, displayName: displayName })
               );
-              navigate("/browse");
             })
             .catch((error) => {
               // An error occurred
@@ -73,8 +70,6 @@ const Login = () => {
           // Signed in
           const user = userCredential.user;
           console.log("Signed in : ", user);
-          navigate("/browse");
-          // ...
         })
         .catch((error) => {
           const errorCode = error.code;
