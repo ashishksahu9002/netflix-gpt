41f7db8e688465706e126d82d1cc10a088c3ccc7
Ashish Kumar <ashishksahu9002@gmail.com>
2025-08-14 17:32:56 +0530

    added font awesome, made video title and background also embeded the trailer video from youtube

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
@@ -8,6 +10,16 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/* 
+          Main Container
+            - Video Background
+            - Video Title
+          Secondary Container
+            - Movie List * n
+              - Movie Card * n
+      */}
     </div>
   );
 };
